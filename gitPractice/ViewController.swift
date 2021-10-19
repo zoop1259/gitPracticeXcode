1810f0f6aeeab8a09ddddddffc85a910da65dee7
zoop1259 <kdm432191@gmail.com>
2021-10-20 04:28:20 +0900

    Initial Commit

---
+++ gitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitPractice
+//
+//  Created by 강대민 on 2021/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
