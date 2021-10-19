234e2e668d7474ce8646cfd22139ec8cb3bca971
zoop1259 <kdm432191@gmail.com>
2021-10-20 04:39:13 +0900

    코드에 두번쨰 브랜치 코드 입력한것.

--- gitPractice/ViewController.swift
+++ gitPractice/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var firstbranch: UILabel!
+    @IBOutlet var secbranch: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
